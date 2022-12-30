c8a5daa8295c412d0b23f17ec4a51f7aa5bca2c9
TeoElRey <teodortopancs@gmail.com>
2022-12-29 21:17:10 -0500

    fixed JSON data request and fixed html display

--- weather/weather.js
+++ weather/weather.js
@@ -1,15 +1,18 @@
 let txt = document.getElementById("textbox");
 let temp = document.getElementById("temp-value");
 let button = document.getElementById("search");
+let request = document.getElementById("city-name");
 let city = '';
-
 button.addEventListener("click", async () => {
   let city = txt.value;
+  city = city.charAt(0).toUpperCase() + city.slice(1)
+  request.innerHTML = city + ':';
   txt.value = '';
   fetch('https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=1adf9c98d1f1ffc3e24690db5e1f1659')
   .then(async function(resp) {const data = await resp.json()
-   console.log(data)})
-
+   var temperature = data.main.temp;
+   var temperature = temperature - 273.15;
+   temp.innerHTML = parseInt(temperature)})
   .catch(function () {
     txt.placeholder = 'Please Enter a Valid Location';
   })
